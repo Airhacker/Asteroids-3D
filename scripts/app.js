24ae9d8f77d0fd966d5c899e5a2301a4751c18c4
Umair Ahmed <umairahmed.dev@gmail.com>
2020-04-23 22:22:04 -0700

    meteor initial

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,13 @@
 // Variables
-let scene, camera, renderer, starGeo, stars;
+let scene, camera, renderer, starGeo, stars, meteorGroup;
+let starAmount = 2500;
 const fov = 60;
 const gameArea = document.querySelector(".game-space");
 
 // THREE.js Initializer
 function init() {
 	scene = new THREE.Scene();
+	scene.fog = new THREE.FogExp2(0xf0fff0, 0.1);
 	// Camera Settings
 	camera = new THREE.PerspectiveCamera(
 		fov,
@@ -30,7 +32,7 @@ function init() {
 
 	// Stars Settings
 	starGeo = new THREE.Geometry();
-	for (let i = 0; i < 2500; i++) {
+	for (let i = 0; i < starAmount; i++) {
 		star = new THREE.Vector3(
 			Math.random() * 600 - 300,
 			Math.random() * 600 - 300,
@@ -39,7 +41,7 @@ function init() {
 		// Star velocity settings
 
 		star.velocity = 0;
-		star.acceleration = 0.02;
+		star.acceleration = 0.05;
 
 		starGeo.vertices.push(star);
 	}
@@ -63,27 +65,74 @@ function init() {
 	});
 
 	//Lights
-	let directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+	let hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.5);
+	scene.add(hemiLight);
 
+	let directionalLight = new THREE.DirectionalLight(0xfefce9, 2);
+	directionalLight.position.set(-5, 2, 0);
 	scene.add(directionalLight);
 
+	// Meteor Loader
+	meteorGeo = new THREE.SphereGeometry(0.2, 8, 6);
+	meteorMat = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+	meteor = new THREE.Mesh(meteorGeo, meteorMat);
+
+	// Meteor Array
+	meteorGroup = new THREE.Object3D();
+	for (let i = 0; i < 100; i++) {
+		meteorGroup.add(meteor);
+	}
+	scene.add(meteorGroup);
+	//meteor.position.set(Math.random() * 600 - 300, 0, -450);
+
 	// Run Animation Loop
-	animate();
+	animate(meteorGroup);
 }
 
-function animate() {
+function animate(group) {
 	starGeo.vertices.forEach((p) => {
 		p.velocity += p.acceleration;
 		p.z += p.velocity;
-		if (p.z > 200) {
-			p.z = -200;
+		if (p.z > 100) {
+			p.z = -450;
 			p.velocity = 0;
 		}
 	});
 	starGeo.verticesNeedUpdate = true;
+
+	// Meteor Movement Settings
+
+	//group.forEach();
+
+	if (meteor.position.z >= 50) {
+		meteor.position.z += 0.25;
+	} else {
+	}
 
 	renderer.render(scene, camera);
 	requestAnimationFrame(animate);
 }
 
 init();
+
+// Key presses
+window.addEventListener("keydown", (e) => {
+	// Checks for left movement
+	if (e.keyCode == 65 || e.keyCode == 37) {
+		if (spaceShip.position.x >= -1) {
+			spaceShip.position.x -= 0.125;
+		}
+		// Checks for right movement
+	} else if (e.keyCode == 68 || e.keyCode == 39) {
+		if (spaceShip.position.x <= 1) {
+			spaceShip.position.x += 0.125;
+		}
+		// Check for Spacebar
+	} else if (e.keyCode == 32) {
+		shipFire();
+	}
+});
+
+function shipFire() {}
+
+function meteors() {}
